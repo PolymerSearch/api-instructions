c1a6dc45359aa23581fd1f15f203a295fece4f37
dineshagrawal85 <dinesh.agrawal85@gmail.com>
2021-09-27 13:15:29 +0530

    adding curl for dataset update

---
+++ dataset_update_curl_sample_ex1.sh
@@ -0,0 +1,7 @@
+curl --location --request PUT 'https://api.polymersearch.com/v1/dataset/6151754dfad3627deeb8f84b' \
+--header 'x-api-key: XXeca66c-21f3-XX39-b407-64e00c62XXXX' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+	"name": "FB Ad List Q2 C-uploaded.csv",
+	"url": "https://test-csv-datasets.s3.us-east-2.amazonaws.com/Test+-+Bank+Loans.csv"
+}'
